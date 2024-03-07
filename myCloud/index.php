b8f80c59e99001d86d42deea273a24e07e88f924
Piotr Zań <73931821+Panmaslak@users.noreply.github.com>
2024-03-07 14:31:41 +0100

    Update index.php

--- myCloud/index.php
+++ myCloud/index.php
@@ -14,7 +14,6 @@
 <head>
     <meta charset="UTF-8">
     <title>MyCloud</title>
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
@@ -45,7 +44,6 @@
         </form>
     </div>
 
-    <!-- Bootstrap JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
